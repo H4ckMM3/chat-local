bc79ddedb9a7f45691e72e77f95223cecb2281aa
HackMe <119747712+H4ckMM3@users.noreply.github.com>
2024-09-24 23:45:03 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// Отдача index.html
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('Пользователь подключился');
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Пользователь отключился');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Сервер работает на порту 3000');
+});
